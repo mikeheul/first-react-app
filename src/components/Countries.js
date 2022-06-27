50efcfb215224c69770ddd52da0604d59bfe197a
Mikeheul <mickael.murmann@gmail.com>
2022-06-28 00:04:50 +0200

    props

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,17 +1,26 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import Card from './Card';
 
 const Countries = () => {
     const [data, setData] = useState([])
     // Le useEffect se joue lorsque le composant est "monté"
     useEffect(() => {
-        axios.get("https://restcountries.com/v3.1/all")
-        .then((res) => console.log(res.data))
+        axios
+            .get("https://restcountries.com/v3.1/all")
+            .then((res) => setData(res.data))
     }, [])
 
     return (
         <div className="countries">
             <h1>Countries</h1>
+            <ul>
+            {
+                data.map((country, index) => (
+                    <Card key={index} country={country} />
+                ))
+            }
+            </ul>
         </div>
     );
 };
