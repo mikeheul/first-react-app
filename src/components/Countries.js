be9df2b0ac85b2a2ff42a1adb569a90213ae88d4
Mikeheul <mickael.murmann@gmail.com>
2022-06-28 00:21:17 +0200

    range countries

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -4,6 +4,7 @@ import Card from './Card';
 
 const Countries = () => {
     const [data, setData] = useState([])
+    const [rangeValue, setRangeValue] = useState(36)
     // Le useEffect se joue lorsque le composant est "monté"
     useEffect(() => {
         axios
@@ -13,10 +14,19 @@ const Countries = () => {
 
     return (
         <div className="countries">
-            <h1>Countries</h1>
+            <ul className="radio-container">
+                <input 
+                    type="range" 
+                    min="1" 
+                    max="250" 
+                    defaultValue={rangeValue}
+                    onChange={(e) => setRangeValue(e.target.value)} />
+            </ul>
             <ul>
             {
-                data.map((country, index) => (
+                data
+                    .slice(0, rangeValue)
+                    .map((country, index) => (
                     <Card key={index} country={country} />
                 ))
             }
